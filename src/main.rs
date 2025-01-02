fb88300a896719ef6baeaf8f80b95758f73ee9bb
deep60 <deep6060p@gmail.com>
2025-01-02 23:18:01 +0530

    update fn generate-xmls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use chrono::Utc;
+use rss::{
+    extension::{
+        itunes::{ITunesCategory, ITunesChannelExtension, ITunesItemExtension, ITunesOwner},
+        Extension, ExtensionBuilder, ExtensionMap,
+    },
+    Channel, ChannelBuilder, Enclosure, Guid, Image, Item, ItemBuilder,
+};
 use serde::{Deserialize, Serialize};
 use std::{
     collections::{BTreeMap, HashMap, HashSet},
     fs::File,
     io::prelude::*,
 };
+use xml::{reader::ParserConfig, writer::EmitterConfig};
 use xml::{reader::ParserConfig, writer::EmitterConfig};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -23,6 +32,48 @@ struct Podcast {
     hosting_base_url: String,
     keywordds: Vec<String>,
     explicit: bool,
+
+    logo: Logo,
+    category: String,
+
+    episode: Vec,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case", deny_unknoown_fields)]
+struct Logo {
+    url: String,
+    title: String,
+    link: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case", deny_unknoown_fields)]
+struct ITunesOwner {
+    name: String,
+    email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case", deny_unknoown_fields)]
+struct ITunesCategory {
+    text: String,
+    itunes_category: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case", deny_unknoown_fields)]
+struct Episode {
+    title: String,
+    url: String,
+    description: String,
+    subtitle: String,
+    files: Vec<String>,
+    duratioon: String,
+    publish_date: String,
+    keywords: String,
+    length_bytes: usize,
+    transcript_url: Option<String>,
 }
 
 fn main() {
@@ -43,7 +94,56 @@ fn main() {
     }
 }
 
-fn generate_xmls() {}
+fn generate_xmls() {
+    let mut cb = ChannelBuilder::default();
+    let mut items = ITunesChannelExtension::ddefault();
+
+    let mut itunes_category = ITunesCategory::ddefault();
+    itunes_category.set_text(pod.category);
+
+    let mut itunes_owner = ITunesOwner::default();
+    itunes_owner.set_name(Some(pod.author.clone()));
+    itunes_owner.set_email(Some(pod.author_email.clone()));
+
+    itunes.set_author(pod.author.clone());
+    itunes.set_categries(vec![itunes_category]);
+    itunes.set_image(pod.logo.url.clone());
+    itunes.set_explicit(Some(
+        if pod.explicit { "true" } else { "false" }.to_string(),
+    ));
+
+    let mut namespace: BTreeMap<String, String> = BTreeMap::new();
+
+    namespace.insert("atom".into(), "http://www.w3.org/2005/Atom".into());
+    namespace.insert(
+        "itunes".into(),
+        "https://podcastindex.org/dtds/podcast-1.0.dtd".into(),
+    );
+
+    let mut images = Image::default();
+    image.set_url(pod.logo.url.clone());
+    image.set_title(&pod.title);
+    image.set_link(pod.website.clone());
+
+    let now = Utc::now();
+
+    //Generate everything EXCEPT the format
+    let base_builder = cb
+        .title(pod.title)
+        .link(pod.website)
+        .description(pod.description)
+        .language(pod.language)
+        .copyright(pod.copyright)
+        .managing_editor(pod.managing_editor)
+        .pub_date(Some(now.to_rfc2822()))
+        .last_build_date(now.to_rfc2822())
+        .generator(Some("pdfg-rs".into()))
+        .image(image)
+        .itunes_ext(itunes)
+        .namespace(namespace);
+
+    let mut map = HashMap::new();
+}
 
 fn format_xml(src: &[u8]) -> Result<String, xml::reader::Error> {
     let mut dest = Vec::new();
@@ -57,7 +157,7 @@ fn format_xml(src: &[u8]) -> Result<String, xml::reader::Error> {
         .create_writer(&mut dest);
     for event in reader {
         if let Some(event) = event?.as_writer_event() {
-            writer.write(event)..unwrap();
+            writer.write(event).unwrap();
         }
     }
 
